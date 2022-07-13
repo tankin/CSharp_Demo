d05c1adb16d202e04ada315997431d4a7b2ea6a6
tankin <tnag@163.com>
2022-07-13 11:25:31 +0800

    Add files via upload

---
+++ TestLiquid/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using DotLiquid;
+
+namespace TestLiquid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // lower case or upper case is all OK
+            string strTemplate = 
+@"<p>{{ user.name }} has to do:</p>
+{% for item in user.tasks %}{{ item.name }}
+{% endfor %}";
+            Console.WriteLine(strTemplate);
+
+            Console.WriteLine("\n-----------------------------------------\n");
+
+            // variables are all upper case
+            var userObj = new User
+            {
+                Name = "Tim Jones",
+                Tasks = new List<Task>
+                {
+                    new Task { Name = "Documentation" },
+                    new Task { Name = "Code comments" }
+                }
+            };
+
+            Template template = Template.Parse(strTemplate);  // Parses and compiles the template
+            string str = template.Render(Hash.FromAnonymousObject(new { user = userObj })); // Renders the output => "hi tobi"
+            Console.WriteLine(str);
+        }
+    }
+
+    public class User : Drop
+    {
+        public string Name { get; set; }
+        public List<Task> Tasks { get; set; }
+    }
+
+    public class Task : Drop
+    {
+        public string Name { get; set; }
+    }
+}
